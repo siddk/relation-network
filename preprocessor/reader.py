1f1ad72debee943d26951b2efc1ff6a1d30702cd
Sidd Karamcheti <sidd.karamcheti@gmail.com>
2017-07-09 02:06:34 -0400

    Add initial ckpt

---
+++ preprocessor/reader.py
@@ -0,0 +1,90 @@
+"""
+reader.py
+
+Reads in and processes the bAbI Task files, serializing them for future reference.
+"""
+import numpy as np
+import os
+import pickle
+import re
+
+PAD_ID, PAD_TOKEN = 0, "<<pad>>"
+SPLIT_RE = re.compile('(\W+)?')
+tokenize = lambda x: [token.strip().lower() for token in re.split(SPLIT_RE, x) if token.strip()]
+
+
+def parse(mode, pik_path, word2id=None, a_word2id=None, max_len=0):
+    if mode == 'train':
+        if os.path.exists(pik_path):
+            with open(pik_path, 'r') as f:
+                S, S_len, Q, Q_len, A, word2id, a_word2id = pickle.load(f)
+                return S, S_len, Q, Q_len, word2id, a_word2id
+        else:
+            # Parse into Stories, Queries, Answers
+            all_stories = []
+            for i in range(1, 21):
+                with open('data/qa%d_train.txt' % i, 'r') as f:
+                    all_stories += parse_stories(f.readlines())
+
+            # Build Vocabulary
+            vocab, answer_vocab, max_s, max_q = set(), set(), 0, 0
+            for s, _, q, _, a in all_stories:
+                for line in s:
+                    vocab.update(line)
+                    max_s = max(max_s, len(line))
+                vocab.update(q)
+                max_q = max(max_q, len(q))
+                answer_vocab.add(a)
+
+            word2id = {w: i for i, w in enumerate([PAD_TOKEN] + list(vocab))}
+            a_word2id = {w: i for i, w in enumerate(list(answer_vocab))}
+            S, S_len, Q, Q_len, A = vectorize(all_stories, word2id, a_word2id, max_s, max_q)
+
+            with open(pik_path, 'w') as f:
+                pickle.dump((S, S_len, Q, Q_len, A, word2id, a_word2id), f)
+
+            return S, S_len, Q, Q_len, A, word2id, a_word2id
+
+
+def parse_stories(lines, truncate=20):
+    """
+    Boilerplate bAbI Task Format Parser.
+    """
+    stories, story = [], []
+    for line in lines:
+        line = line.strip()
+        nid, line = line.split(' ', 1)
+        nid = int(nid)
+        if nid == 1:
+            story = []
+        if '\t' in line:
+            query, answer, supporting = line.split('\t')
+            query = tokenize(query)
+            sub_story = [x for x in story if x][-truncate:]
+            sub_story_len = [len(x) for x in sub_story]
+            stories.append((sub_story, sub_story_len, query, len(query), answer.lower()))
+            story.append('')
+        else:
+            sentence = tokenize(line)
+            story.append(sentence)
+    return stories
+
+
+def vectorize(s, word2id, answer_word2id, max_s, max_q, s_len=20):
+    """
+    Vectorize raw stories, using the provided general and answer-specific vocabularies.
+    """
+    S, Q, A = np.zeros([len(s), s_len, max_s], int), np.zeros([len(s), max_q], int), np.zeros([len(s)], int)
+    S_len, Q_len = np.zeros([len(s), s_len], int), np.zeros([len(s)], int)
+    for i in range(len(s)):
+        story, story_len, query, query_len, answer = s[i]
+        for j in range(len(story)):
+            for k in range(len(story[j])):
+                S[i][j][k] = word2id[story[j][k]]
+        for j in range(len(story_len)):
+            S_len[i][j] = story_len[j]
+        for j in range(len(query)):
+            Q[i][j] = word2id[query[j]]
+        Q_len[i] = query_len
+        A = answer_word2id[answer]
+    return S, S_len, Q, Q_len, A
--- run.py
+++ run.py
@@ -3,6 +3,8 @@ run.py
 
 Core script for building, training, and evaluating a Relation Network on the bAbI Tasks Dataset (10k Joint-Training).
 """
+from preprocessor.reader import parse
+import os
 import tensorflow as tf
 
 FLAGS = tf.app.flags.FLAGS
@@ -14,16 +16,14 @@ tf.app.flags.DEFINE_string("ckpt_dir", "ckpt/", "Directory to store checkpoints,
 # Train Mode Parameters
 
 
-
 def main(_):
     if FLAGS.mode == "train":
-        pass
+        S, S_len, Q, Q_len, A, word2id, a_word2id = parse("train", pik_path=os.path.join(FLAGS.ckpt_dir, 'train.pik'))
     elif FLAGS.mode == "eval":
         pass
     else:
         print "Unsupported Mode, use one of [train, eval]"
         raise UserWarning
-
 
 
 if __name__ == "__main__":
